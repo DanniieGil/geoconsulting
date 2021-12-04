1502962fbe0ed56bebde84ea2f740ef05d8584d6
DanniieGil <danniegil@hotmail.com>
2021-12-04 06:01:03 -0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>En mantenimiento...</p>
-				Contacto:
-				<a
-					className="App-link"
-					href="#"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					info@geoconsulting.com.co
-				</a>
-			</header>
-		</div>
-	);
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>En mantenimientos...</p>
+        Contacto:
+        <a
+          className="App-link"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          info@geoconsulting.com.co
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
