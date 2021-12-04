8c8c296aa03839a0bdce9ea63318d29f6a31e073
DanniieGil <danniegil@hotmail.com>
2021-12-04 06:06:46 -0500

    test2

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>En mantenimientos...</p>
+        <p>En mantenimientosz...</p>
         Contacto:
         <a
           className="App-link"
